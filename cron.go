efe2179fb7832991e978e90ca54519f744fa0cdf
Philipp Gruber <philipp.gruber@flupps.net>
2024-02-08 22:45:35 +1100

    load location from config

--- cron.go
+++ cron.go
@@ -10,17 +10,57 @@ import (
     "strconv"
 )
 
+func loadConfig(configFile string) (float64, float64, string) {
+    file, err := os.Open(configFile)
+    if err != nil {
+        panic(err)
+    }
+    scanner := bufio.NewScanner(file)
+    scanner.Split(bufio.ScanLines);
+    var line string
+    var lat, lng float64
+    var timezone string
+    for scanner.Scan() {
+        line = scanner.Text()
+        line = strings.TrimSpace(line)
+        if !strings.Contains(line, "=") {
+            continue
+        }
+        kv := strings.Split(line, "=")
+        if (kv[0] == "timezone") {
+            timezone = kv[1]
+        }
+        if (kv[0] == "location") {
+            if !strings.Contains(kv[1], ",") {
+            fmt.Println("Invalid coordinates: " + kv[1])
+                continue
+            }
+            ll := strings.Split(kv[1], ",")
+            lat, _ = strconv.ParseFloat(ll[0], 32)
+            lng, _ = strconv.ParseFloat(ll[1], 32)
+        }
+    }
+    return lat, lng, timezone
+}
+
+
 func main() {
     sunCronFilePath := "sun.cron"
+    configFile := "/etc/suncron.conf"
+
+    lat, lng, timezone := loadConfig(configFile)
 
     today := time.Now()
     sunrise, sunset := sunrise.SunriseSunset(
-        -37.864, 144.982,
+        lat, lng,
         today.Year(), today.Month(), today.Day(),
     )
-    tz, _ := time.LoadLocation("Australia/Melbourne")
+    tz, err := time.LoadLocation(timezone)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
     sunrise.In(tz)
-    fmt.Println("Location:", today.Location(), ":Time:", today)
     fmt.Println("Sunrise: " + sunrise.In(tz).Format("15:04:05"))
     fmt.Println("Sunset: " + sunset.In(tz).Format("15:04:05"))
 
@@ -31,21 +71,20 @@ func main() {
     scanner := bufio.NewScanner(file)
     scanner.Split(bufio.ScanLines);
 
-    fmt.Println("Reading " + sunCronFilePath)
     var hour int
     var minute int
 
     for scanner.Scan() {
         line := scanner.Text()
-        fmt.Println("Line: " + line)
         r := strings.Split(line, ";")
-        if len(r) != 3 {
+        if len(r) != 4 {
             fmt.Println("Error: Invalid entry: " + line)
             continue
         }
-        base := strings.Trim(r[0], " ")
+        base    := strings.Trim(r[0], " ")
         inptime := strings.Trim(r[1], " ")
-        cmd  := strings.Trim(r[2], " ")
+        dmw     := strings.Trim(r[2], " ")
+        cmd     := strings.Trim(r[3], " ")
         if base != "sunrise" && base != "sunset" {
             fmt.Println("Error: Invalid value: " + base)
             continue
@@ -83,8 +122,7 @@ func main() {
         }
         endTime = endTime.Add(time.Hour * time.Duration(hour * mod) + time.Minute * time.Duration(minute * mod))
 
-        fmt.Printf("result: %s %d:%d => %s: %s\n", base, hour, minute, endTime, cmd)
-        fmt.Printf("%d %d * * * %s\n", endTime.Minute(), endTime.Hour(), cmd)
+        fmt.Printf("%d %d %s %s\n", endTime.Minute(), endTime.Hour(), dmw, cmd)
     }
 
     file.Close()
